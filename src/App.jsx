2953b702d14d652b6d6389453c94833a9e5c63a1
Kushal Bansal <kushal.bansal.1001@gmail.com>
2024-09-11 13:10:58 +0545

    images extracted

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-export default function App() {
+import React, { useState } from "react";
+
+const App = () => {
+
+  // current Index and max Index
+  const [vals,serVals] = useState({currentIndex: 0, maxIndex: 422});
+
+  //preload images in the background
+
+  //load images in the background
+  //resize canvas
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div className="w-full bg-zinc-900 ">
+      <div className="w-full h-[400vh]">
+        <div className="w-full h-screen sticky left-0 top-0 ">
+          <canvas className="w-full h-screen "></canvas>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
